57118b79a226619809714234162f82b57a5649f4
Anil Kumar Gola <anil.gola@tothenew.com>
2026-01-16 21:16:54 +0530

    commit

---
+++ src/main/java/org/anil/FinalExcercise.java
@@ -0,0 +1,12 @@
+package org.anil;
+
+public class FinalExcercise {
+
+    static void main() {
+        // we have 100 applications
+        // each applications need to processed in parallel
+        // each application needs to fetch information from 5 entities in paraller
+        // once information is fetched from 5 entities, need to combine results
+        // need to do as much parallelism as necessary
+    }
+}
--- src/main/java/org/anil/StructuredConcurrency.java
+++ src/main/java/org/anil/StructuredConcurrency.java
@@ -1,66 +1,15 @@
 package org.anil;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.StructuredTaskScope;
+import java.util.concurrent.TimeoutException;
 
 public class StructuredConcurrency {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-
-        // seller data
-        // seller score
-        // combine
-
-//
-//         1000 application
-//             --> pancardauthority
-//             --> aadhar authority
-//             --> i need to calculate score once above two finish
-
-        Thread[] threads = new Thread[10];
-      for(int i=0; i<10; i++){
-          int iteration = i;
-
-          threads[i] = Thread.startVirtualThread(()->{
-
-              try(var scope = new StructuredTaskScope.ShutdownOnFailure())
-              {
-
-
-                  var task1 = scope.fork(() -> {
-                       if(iteration == 5){
-                           throw new RuntimeException("exception in 500");
-                       }
-                      return "pancardauthority";
-                  });
-
-                  var task2 = scope.fork(() -> {
-                      return "adhar data";
-                  });
-
-                  scope.join();
-                   scope.throwIfFailed();
-
-                  var result1 = task1.get();
-                  var result2 = task2.get();
-
-                  System.out.println(result1 + " " + result2);
-
-              }
-              catch (Exception ex){
-                  System.out.println(ex);
-              }
-
-          });
-
-
-
-
-      }
-
-
-for(Thread thread : threads){
-    thread.join();
-}
 
 
     }
